caa73df9a223018a128c67f6c5deade61c2fa0c4
wangyihuai <wangyihuai1997@foxmail.com>
2018-11-07 15:42:12 +0800

    项目初始化

---
+++ src/main/java/cn/com/pism/pismos/commons/pismcommons/PismCommonsApplication.java
@@ -0,0 +1,12 @@
+package cn.com.pism.pismos.commons.pismcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PismCommonsApplication.class, args);
+	}
+}
